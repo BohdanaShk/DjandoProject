a5e94eddbef00e194d552b496282ba67509bbf17
Bohdana <bogdana.shkumbatiuk@gmai.com>
2022-02-02 16:29:15 +0200

    migration

--- Kitchen/models.py
+++ Kitchen/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from FoodingHelper import settings
+from Kitchen.views import User
+
+
+class Ingredients(models.Model):
+    product = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class KitchenModels(models.Model):
+    name_dishes = models.CharField(max_length=255)
+    cooking = models.TextField
+    ingredients = models.ForeignKey(Ingredients, unique=False, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='photos/%y/%m/%d', blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
--- Kitchen/views.py
+++ Kitchen/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+def index(request):
+    return HttpResponse('PAGE 1')
+
+
+def about(request):
+    return HttpResponse('PAGE 2')
